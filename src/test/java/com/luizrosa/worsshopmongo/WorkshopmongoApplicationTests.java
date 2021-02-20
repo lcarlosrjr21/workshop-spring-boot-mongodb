7207a9c6db91ae0e599a56dfe21779d9c500de43
Luiz Rosa <lcarlosrjr@gmail.com>
2021-02-20 11:25:50 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/luizrosa/worsshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizrosa.worsshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
