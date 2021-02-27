c7cdcb565b5d1b990e87bc84353291a0a7b520a1
Luiz Rosa <lcarlosrjr@gmail.com>
2021-02-27 16:20:56 -0300

    Obtendo um post por id

---
+++ src/main/java/com/luizrosa/worsshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.luizrosa.worsshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luizrosa.worsshopmongo.domain.Post;
+import com.luizrosa.worsshopmongo.repository.PostRepository;
+import com.luizrosa.worsshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo; // dependencia para o banco de dados
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
