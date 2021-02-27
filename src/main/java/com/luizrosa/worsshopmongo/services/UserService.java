7782bdf37bc6871ae77d1bf72ba457f534b617b5
Luiz Rosa <lcarlosrjr@gmail.com>
2021-02-27 15:18:36 -0300

    Atualização de usuário com PUT

--- src/main/java/com/luizrosa/worsshopmongo/services/UserService.java
+++ src/main/java/com/luizrosa/worsshopmongo/services/UserService.java
@@ -29,15 +29,25 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
-	
-	
+
 }
