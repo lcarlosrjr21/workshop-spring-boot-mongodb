c576185b82b06d09d586f14d67d136331595abd0
Luiz Rosa <lcarlosrjr@gmail.com>
2021-02-27 16:09:47 -0300

    Referenciando os posts do usuário

--- src/main/java/com/luizrosa/worsshopmongo/config/Instantiation.java
+++ src/main/java/com/luizrosa/worsshopmongo/config/Instantiation.java
@@ -40,5 +40,8 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("27/02/2021"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("28/02/2021"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 	}
 }
