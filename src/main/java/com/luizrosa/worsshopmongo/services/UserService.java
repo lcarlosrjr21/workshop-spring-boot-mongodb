db44de6aa01f38a5d458a69784c59cfe0ff341e5
Luiz Rosa <lcarlosrjr@gmail.com>
2021-02-27 15:01:13 -0300

    Inserção de usuário com POST

--- src/main/java/com/luizrosa/worsshopmongo/services/UserService.java
+++ src/main/java/com/luizrosa/worsshopmongo/services/UserService.java
@@ -7,21 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luizrosa.worsshopmongo.domain.User;
+import com.luizrosa.worsshopmongo.dto.UserDTO;
 import com.luizrosa.worsshopmongo.repository.UserRepository;
 import com.luizrosa.worsshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
-	private UserRepository repo;
-	
-	public List<User> findAll(){
+	private UserRepository repo; // dependencia para o banco de dados
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}
 	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
+}
