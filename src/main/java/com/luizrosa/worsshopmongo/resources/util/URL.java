c40946c8bd45ff9eea1401220a665f1d8f79e36b
Luiz Rosa <lcarlosrjr@gmail.com>
2021-02-28 10:39:48 -0300

    Consulta simples com query methods

---
+++ src/main/java/com/luizrosa/worsshopmongo/resources/util/URL.java
@@ -0,0 +1,17 @@
+package com.luizrosa.worsshopmongo.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+
+	public static String decodeParam(String text) {
+		try {
+			return URLDecoder.decode(text, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+	// no metodo acima sugestao a IDE sugeriu throws UnsupportedEncodingException
+	// na aula aparece o
+}
